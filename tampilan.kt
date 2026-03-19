0d1e6e3e971df052ac32471d5d8c73d23d3997da
juken8000 <arkhanalhakim71@gmail.com>
2026-03-20 00:33:05 +0700

    Add files via upload

---
+++ tampilan.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.model
+
+data class Mangan(
+    val nama: String,
+    val deskripsi: String,
+    val harga: Int,
+    val kalori: Int,
+    val protein: Int,
+    val ImageRes: Int
+)
